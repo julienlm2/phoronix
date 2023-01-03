76c71396400fcf3d4f258cc30a35904275d17d74
julienlm2 <julien.lemoal@hotmail.fr>
2023-01-03 16:42:35 +0100

    utilisation de tout les threads

--- pibench.py
+++ pibench.py
@@ -1,26 +1,35 @@
 #!/usr/bin/env python
+
+from multiprocessing import Pool
+from multiprocessing import cpu_count
 import time
 import math
 import sys
 
 start = time.time()
-tab = [] 
+
 
 if len(sys.argv) == 1  or len(sys.argv) > 2:
     print("Erreur, Le Script prend 1 parametre (nombre d'itérations)")
     exit()
 
-iterations = int(sys.argv[1])
-for i in range(0,iterations):
-    for x in range(1,1000):
-      3.141592 * 2**x
-    for x in range(1,10000):
-      float(x) / 3.141592
-    for x in range(1,10000):
-      float(3.141592) / x
+def bench(x):
+    iterations = int(sys.argv[1])
+    for i in range(0,iterations):
+        for x in range(1,1000):
+            3.141592 * 2**x
+        for x in range(1,10000):
+            float(x) / 3.141592
+        for x in range(1,10000):
+            float(3.141592) / x
 
-end = time.time()
-
-
-duration = round(end - start,3)
-print(f"Time taken: {duration}")
+if __name__ == '__main__':
+    processes = cpu_count()
+    print ('utilizing %d cores\n' % processes)
+    pool = Pool(processes)
+    pool.map(bench, range(processes))
+    pool.close()
+    pool.join()
+    end = time.time()
+    duration = round(end - start,3)
+    print(f"Time taken: {duration}")
